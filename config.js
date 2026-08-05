d1e73c33284bb06e32c0378ad5b6b8fccedcb079
turraa <joratyra161@yandex.ru>
2026-08-05 19:21:29 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window.TURRA_CONFIG = {
+  apiBase: 'https://cab.turrasup.net/api',
+  botUsername: 'TurraVpnbot',
+  siteUrl: 'https://buy.turrasup.ru',
+  deepLinkScheme: 'turravpn',
+  pollIntervalMs: 3000,
+  paymentPollMaxMs: 1200000,
+}
